6320c2969ee9000706a7ca5795f08ff1d7cfcd87
Furman <namruffproductions@gmail.com>
2024-12-10 09:56:11 -0500

    Initial Commit

---
+++ GIT HUB EXAMPLE/GIT_HUB_EXAMPLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_HUB_EXAMPLEApp.swift
+//  GIT HUB EXAMPLE
+//
+//  Created by Furman Barnes on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GIT_HUB_EXAMPLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GIT HUB EXAMPLETests/GIT_HUB_EXAMPLETests.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_HUB_EXAMPLETests.swift
+//  GIT HUB EXAMPLETests
+//
+//  Created by Furman Barnes on 12/10/24.
+//
+
+import Testing
+@testable import GIT_HUB_EXAMPLE
+
+struct GIT_HUB_EXAMPLETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
